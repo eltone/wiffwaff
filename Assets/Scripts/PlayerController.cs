a33407eb2d6e4d82c2b33e580dfd7948ad2d6ce4
eltone <anthonyhobbs@gmail.com>
2015-07-19 16:08:37 +0100

    add paddle and move with mouse

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	void Update() {
+		var horiz = Input.GetAxis ("Mouse X");
+		var vertical = Input.GetAxis ("Mouse Y");
+		var vec = new Vector3 (horiz, 0, vertical);
+		transform.Translate (vec);
+	}
+}
