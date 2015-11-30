9d6338c7a9632907e7948f5105b530290c8c0031
eltone <anthonyhobbs@gmail.com>
2015-11-30 22:56:22 +0000

    got a ball bouncing off a paddle

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPhysics : MonoBehaviour {
+	public float thrust = 200;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			rb.AddForce(transform.forward * thrust);
+		}
+	}
+}
