adce741219762e09f975a0ed07778f9556da9244
eltone <anthonyhobbs@gmail.com>
2015-12-03 22:42:05 +0000

    adjust walls on parent object instead of individually

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	void Update() {
+	void Update()
+    {
         var zAxixPos = -12.9f;
         var normVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zAxixPos - Camera.main.transform.position.z);
         var mouse = Camera.main.ScreenToWorldPoint(normVec);
 		transform.localPosition = new Vector3(mouse.x, mouse.y, zAxixPos);
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision);
+    }
 }
