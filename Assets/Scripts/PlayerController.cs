c9c7bce3b8b58746a16b215531330147f28e3c23
eltone <anthonyhobbs@gmail.com>
2015-12-03 22:50:42 +0000

    move controller rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
 
-	void Update()
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate()
     {
         var zAxixPos = -12.9f;
         var normVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zAxixPos - Camera.main.transform.position.z);
         var mouse = Camera.main.ScreenToWorldPoint(normVec);
-		transform.localPosition = new Vector3(mouse.x, mouse.y, zAxixPos);
+		rb.MovePosition(new Vector3(mouse.x, mouse.y, zAxixPos));
 	}
-
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision);
-    }
 }
