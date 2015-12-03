63ab9ad9cb1c801c438db9b65d7603ab6b319237
eltone <anthonyhobbs@gmail.com>
2015-12-03 22:24:34 +0000

    paddle now matches mouse position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    float sensitivity = 0.5f;
-
 	void Update() {
-		var horiz = Input.GetAxis ("Mouse X");
-		var vertical = Input.GetAxis ("Mouse Y");
-		var vec = new Vector3 (horiz*sensitivity, vertical*sensitivity);
-		transform.Translate (vec);
+        var zAxixPos = -12.9f;
+        var normVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zAxixPos - Camera.main.transform.position.z);
+        var mouse = Camera.main.ScreenToWorldPoint(normVec);
+		transform.localPosition = new Vector3(mouse.x, mouse.y, zAxixPos);
 	}
 }
