30e75ffc53ecd48331c569435578e63de98d5894
eltone <anthonyhobbs@gmail.com>
2017-06-03 15:43:56 +0100

    clamp player paddle to inside box

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -2,18 +2,18 @@
 using System.Collections;
 
 public class BallPhysics : MonoBehaviour {
-	public float thrust = 600;
-	public Rigidbody rb;
+	public float thrust = 1200;
+	private Rigidbody _rb;
     private bool inMotion = false;
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();	
+		_rb = GetComponent<Rigidbody> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButton (0) && !inMotion) {
-			rb.AddForce(transform.forward * thrust);
+			_rb.AddForce(transform.forward * thrust);
             inMotion = true;
 		}
 	}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
+	private Rigidbody _rb;
+	public GameObject backWall;
+	private Vector3 _backWallSize;
+	private Vector3 _playerSize;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+		_backWallSize = backWall.GetComponent<Collider>().bounds.size;
+		_playerSize = GetComponent<Collider>().bounds.size;
     }
 
 	void FixedUpdate()
     {
         var zAxixPos = -12.9f;
         var normVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zAxixPos - Camera.main.transform.position.z);
-        var mouse = Camera.main.ScreenToWorldPoint(normVec);
-		rb.MovePosition(new Vector3(mouse.x, mouse.y, zAxixPos));
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(normVec);
+		_rb.MovePosition(clampInsideBox(mouse, zAxixPos));
+	}
+
+	private Vector3 clampInsideBox(Vector3 worldMouse, float zAxisPos)
+	{
+		var space = (_backWallSize - _playerSize)/ 2;
+		return new Vector3(Mathf.Clamp(worldMouse.x, -space.x, space.x), Mathf.Clamp(worldMouse.y, -space.y, space.y), zAxisPos);
 	}
 }
