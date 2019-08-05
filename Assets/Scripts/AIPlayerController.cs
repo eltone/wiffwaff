18ad97b2df4672be0f94093c03d389aeb0ea4dba
eltone <anthonyhobbs@gmail.com>
2019-08-05 23:31:14 +0100

    add basic AI controlled player 2

---
+++ Assets/Scripts/AIPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPlayerController : PlayerController
+{
+    public float MovementSpeed;
+    public GameObject Ball;
+
+    public void FixedUpdate()
+    {
+        Vector3 ballPos = Ball.transform.position;
+        Vector3 playerPos = transform.position;
+        Vector2 movement = Vector2.MoveTowards(playerPos, ballPos, MovementSpeed * Time.fixedDeltaTime);
+        MovePlayer(movement);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
 {
 	public GameObject LeftWall;
 	public GameObject BottomWall;
-	private Rigidbody _rb;
+	protected Rigidbody Rigidbody;
 	private Collider _pc;
 	private Vector3 _backWallSize;
 	private Vector3 _playerSize;
@@ -15,20 +15,20 @@ public class PlayerController : MonoBehaviour
 		var leftHeight = LeftWall.GetComponent<Renderer>().bounds.size.y;
 		var bottomWidth = BottomWall.GetComponent<Renderer>().bounds.size.x;
 		_backWallSize = new Vector3(bottomWidth, leftHeight, 0);
-		_rb = GetComponent<Rigidbody>();
+		Rigidbody = GetComponent<Rigidbody>();
 		_pc = GetComponent<Collider>();
 		_playerSize = _pc.bounds.size;
-		ZAxisPos = _rb.transform.position.z;
+		ZAxisPos = Rigidbody.transform.position.z;
     }
 
     protected float ZAxisPos;
 
 	protected void MovePlayer(Vector3 target)
 	{
-		_rb.MovePosition(clampInsideBox(target, ZAxisPos));
+		Rigidbody.MovePosition(ClampInsideBox(target, ZAxisPos));
 	}
 
-	private Vector3 clampInsideBox(Vector3 worldMouse, float zAxisPos)
+	private Vector3 ClampInsideBox(Vector3 worldMouse, float zAxisPos)
 	{
 		Vector2 space = (_backWallSize - _playerSize)/ 2;
 		return new Vector3(Mathf.Clamp(worldMouse.x, -space.x, space.x), Mathf.Clamp(worldMouse.y, -space.y, space.y), zAxisPos);
